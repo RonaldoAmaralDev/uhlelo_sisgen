1633077c348b24f32bb52b7a99f24a043ad2d994
Ronaldo Gonçalves Amaral <ronaldocotemig@gmail.com>
2024-05-01 19:14:30 -0300

    Desenvolvimento tela de escolha de modulos.

---
+++ resources/views/admin/pages/index.blade.php
@@ -0,0 +1,221 @@
+
+<!DOCTYPE html>
+<html lang="en" dir="ltr" data-nav-layout="vertical" data-theme-mode="light" data-header-styles="light" data-menu-styles="dark" data-toggled="close">
+    <head>
+
+        <!-- Meta Data -->
+        <meta charset="UTF-8">
+        <meta name='viewport' content='width=device-width, initial-scale=1.0'>
+        <meta http-equiv="X-UA-Compatible" content="IE=edge">
+        <meta name="Description" content="Laravel Bootstrap Responsive Admin Web Dashboard Template">
+        <meta name="Author" content="Spruko Technologies Private Limited">
+        <meta name="keywords" content="laravel admin panel, admin template, admin dashboard, admin, admin panel, laravel template, laravel admin, dashboard, admin dashboard template, admin panel template, template dashboard, bootstrap dashboard, bootstrap admin template, laravel dashboard, dashboard bootstrap 5">
+    
+		<!-- TITLE -->
+        <title> Synto - Laravel Bootstrap 5 Premium Admin & Dashboard Template </title>
+
+        <!-- Favicon -->
+        <link rel="icon" href="https://laravelui.spruko.com/synto/build/assets/img/brand-logos/favicon.ico" type="image/x-icon">
+
+        <!-- ICONS CSS -->
+        <link href="https://laravelui.spruko.com/synto/build/assets/icon-fonts/icons.css" rel="stylesheet">
+
+        <!-- Choices JS -->
+        <script src="https://laravelui.spruko.com/synto/build/assets/libs/choices.js/public/assets/scripts/choices.min.js"></script>
+
+        <!-- Bootstrap Css -->
+        <link id="style" href="https://laravelui.spruko.com/synto/build/assets/libs/bootstrap/css/bootstrap.min.css" rel="stylesheet" >
+
+        <!-- Node Waves Css -->
+        <link href="https://laravelui.spruko.com/synto/build/assets/libs/node-waves/waves.min.css" rel="stylesheet" > 
+
+        <!-- Simplebar Css -->
+        <link href="https://laravelui.spruko.com/synto/build/assets/libs/simplebar/simplebar.min.css" rel="stylesheet" >
+
+        <!-- Color Picker Css -->
+        <link rel="stylesheet" href="https://laravelui.spruko.com/synto/build/assets/libs/flatpickr/flatpickr.min.css">
+        <link rel="stylesheet" href="https://laravelui.spruko.com/synto/build/assets/libs/@simonwep/pickr/themes/nano.min.css">
+
+        <!-- Choices Css -->
+        <link rel="stylesheet" href="https://laravelui.spruko.com/synto/build/assets/libs/choices.js/public/assets/styles/choices.min.css">
+        <!-- APP CSS & APP SCSS -->
+        <link rel="preload" as="style" href="https://laravelui.spruko.com/synto/build/assets/app-C3rLMrk5.css" /><link rel="stylesheet" href="https://laravelui.spruko.com/synto/build/assets/app-C3rLMrk5.css" />      
+        <!-- Main Theme Js -->
+        <script src="https://laravelui.spruko.com/synto/build/assets/main.js"></script>
+
+        
+
+
+
+    </head>
+
+    <body class="">
+
+        <!-- Loader -->
+        <div id="loader" >
+            <img src="https://laravelui.spruko.com/synto/build/assets/img/media/loader.svg" alt="">
+        </div>
+        <!-- Loader -->
+
+        <div class="page">
+
+                <!--APP-CONTENT START-->
+                <div class="main-content app-content">
+                    <div class="container-fluid">
+
+                        <div class="row">
+                            <div class="col-xxl-6 col-xl-6 col-lg-12">
+                                <h6 class="mb-3">Qual modúlo deseja acessar do sistema:</h6>
+
+                                <div class="row row-cols-8">
+
+                                    <div class="col">
+                                        <div class="card custom-card">
+                                            <div class="card-body">
+                                                <img src="https://laravelui.spruko.com/synto/build/assets/img/media/4.jpg" class="card-img mb-3" alt="...">
+                                                <h6 class="card-title fw-semibold">Oficina</h6>
+                                                <p class="card-text">With supporting text below as a natural
+                                                    lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col">
+                                        <div class="card custom-card">
+                                            <div class="card-body">
+                                                <img src="https://laravelui.spruko.com/synto/build/assets/img/media/5.jpg" class="card-img mb-3" alt="...">
+                                                <h6 class="card-title fw-semibold">Caminhões</h6>
+                                                <p class="card-text">With supporting text below as a natural
+                                                    lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+
+                                <div class="row row-cols-8">
+                                    <div class="col">
+                                        <div class="card custom-card">
+                                            <div class="card-body">
+                                                <img src="https://laravelui.spruko.com/synto/build/assets/img/media/6.jpg" class="card-img mb-3" alt="...">
+                                                <h6 class="card-title fw-semibold">Linha Amarela</h6>
+                                                <p class="card-text">With supporting text below as a natural
+                                                    lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col">
+                                        <div class="card custom-card">
+                                            <div class="card-body">
+                                                <img src="https://laravelui.spruko.com/synto/build/assets/img/media/6.jpg" class="card-img mb-3" alt="...">
+                                                <h6 class="card-title fw-semibold">Horímetro</h6>
+                                                <p class="card-text">With supporting text below as a natural
+                                                    lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    </div>
+                                    <div class="row row-cols-12">
+
+                                    <div class="col">
+                                        <div class="card custom-card">
+                                            <div class="card-body">
+                                                <img src="https://laravelui.spruko.com/synto/build/assets/img/media/6.jpg" class="card-img mb-3" alt="...">
+                                                <h6 class="card-title fw-semibold">Borracharia</h6>
+                                                <p class="card-text">With supporting text below as a natural
+                                                    lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col">
+                                        <div class="card custom-card">
+                                            <div class="card-body">
+                                                <img src="https://laravelui.spruko.com/synto/build/assets/img/media/6.jpg" class="card-img mb-3" alt="...">
+                                                <h6 class="card-title fw-semibold">Ferramentaria</h6>
+                                                <p class="card-text">With supporting text below as a natural
+                                                    lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col">
+                                        <div class="card custom-card">
+                                            <div class="card-body">
+                                                <img src="https://laravelui.spruko.com/synto/build/assets/img/media/6.jpg" class="card-img mb-3" alt="...">
+                                                <h6 class="card-title fw-semibold">Portaria</h6>
+                                                <p class="card-text">With supporting text below as a natural
+                                                    lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                    <div class="col">
+                                        <div class="card custom-card">
+                                            <div class="card-body">
+                                                <img src="https://laravelui.spruko.com/synto/build/assets/img/media/6.jpg" class="card-img mb-3" alt="...">
+                                                <h6 class="card-title fw-semibold">Suporte</h6>
+                                                <p class="card-text">With supporting text below as a natural
+                                                    lead-in to additional content.</p>
+                                            </div>
+                                        </div>
+                                    </div>
+
+                                </div>
+                            </div>
+                        </div>
+
+                    </div>
+                </div>
+                <!--APP-CONTENT CLOSE-->
+
+            <footer class="footer mt-auto py-3 bg-white text-center">
+				<div class="container">
+					<span class="text-center">Copyright © <span id="year">2024</span>
+						<a href="javascript:void(0)" class="text-primary">SISGEN</a>. Desenvolvido <span class="ri ri-heart-fill text-danger"></span> por
+						<a class="text-primary" href="javascript:void(0)"> UHLELO </a> Todos os direitos reservados. </span>
+				</div>
+			</footer>            
+
+              
+
+        </div>
+
+        <!-- SCRIPTS -->
+        <!-- Scroll To Top -->
+         <div class="scrollToTop">
+            <span class="arrow"><i class="ri-arrow-up-s-fill fs-20"></i></span>
+         </div>
+         <div id="responsive-overlay"></div>
+         <!-- Scroll To Top -->
+
+         <!-- Popper JS -->
+         <script src="https://laravelui.spruko.com/synto/build/assets/libs/@popperjs/core/umd/popper.min.js"></script>
+
+         <!-- Bootstrap JS -->
+         <script src="https://laravelui.spruko.com/synto/build/assets/libs/bootstrap/js/bootstrap.bundle.min.js"></script>
+
+         <!-- Node Waves JS-->
+         <script src="https://laravelui.spruko.com/synto/build/assets/libs/node-waves/waves.min.js"></script>
+
+         <!-- Simplebar JS -->
+         <script src="https://laravelui.spruko.com/synto/build/assets/libs/simplebar/simplebar.min.js"></script>
+         <link rel="modulepreload" href="https://laravelui.spruko.com/synto/build/assets/simplebar-B35Aj-bA.js" /><script type="module" src="https://laravelui.spruko.com/synto/build/assets/simplebar-B35Aj-bA.js"></script>
+         <!-- Color Picker JS -->
+         <script src="https://laravelui.spruko.com/synto/build/assets/libs/@simonwep/pickr/pickr.es5.min.js"></script>
+
+         	
+        <!-- Sticky JS -->
+        <script src="https://laravelui.spruko.com/synto/build/assets/sticky.js"></script>
+
+        <!-- Custom-Switcher JS -->
+        <link rel="modulepreload" href="https://laravelui.spruko.com/synto/build/assets/custom-switcher-oPg2q12r.js" /><script type="module" src="https://laravelui.spruko.com/synto/build/assets/custom-switcher-oPg2q12r.js"></script>
+        <!-- APP JS-->
+		<link rel="modulepreload" href="https://laravelui.spruko.com/synto/build/assets/app-DhXvUdFt.js" /><script type="module" src="https://laravelui.spruko.com/synto/build/assets/app-DhXvUdFt.js"></script>       
+        <!-- END SCRIPTS -->
+
+    </body> 
+
+</html>
